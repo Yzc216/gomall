003dfb14d1a1111db9abec1e8a3ecb260e6b445e
yzc <yangzc216@163.com>
2025-02-07 16:53:29 +0800

    suite update

--- common/clientsuite/clientsuite.go
+++ common/clientsuite/clientsuite.go
@@ -7,28 +7,35 @@ import (
 	"github.com/cloudwego/kitex/transport"
 	"github.com/hertz-contrib/obs-opentelemetry/provider"
 	"github.com/kitex-contrib/obs-opentelemetry/tracing"
+	consul "github.com/kitex-contrib/registry-consul"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 )
 
 type CommonGrpcClientSuite struct {
-	DestServiceName    string
-	DestServiceAddr    string
+	//DestServiceName    string
+	//DestServiceAddr    string
 	CurrentServiceName string
+	RegistryAddr       string
 	TracerProvider     *tracesdk.TracerProvider
 }
 
 func (s CommonGrpcClientSuite) Options() []client.Option {
 	opts := []client.Option{
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{
+			ServiceName: s.CurrentServiceName,
+		}),
 		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
 		client.WithTransportProtocol(transport.GRPC),
 	}
 
 	_ = provider.NewOpenTelemetryProvider(provider.WithSdkTracerProvider(s.TracerProvider), provider.WithEnableMetrics(false))
 
+	r, err := consul.NewConsulResolver(s.RegistryAddr)
+	if err != nil {
+		panic(err)
+	}
 	opts = append(opts,
-		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{
-			ServiceName: s.CurrentServiceName,
-		}),
+		client.WithResolver(r),
 		client.WithSuite(tracing.NewClientSuite()),
 	)
 
